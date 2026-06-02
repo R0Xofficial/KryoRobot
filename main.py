a623b93be027d120359724e750b24dcd1ea74f9a
R0X <patryk.bieganski1003@gmail.com>
2026-06-02 11:30:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -62,6 +62,18 @@ async def support_callback_handler(update: Update, context: ContextTypes.DEFAULT
     approver_link = await utils.create_user_link(admin.id, context)
     curr_time = utils.get_utc_now()
 
+    try:
+        chat_obj = await context.bot.get_chat(chat_id)
+        
+        if chat_obj.type == ChatType.PRIVATE:
+            supporter_obj = await context.bot.get_chat(supporter_id)
+            chat_display = f"PM with {utils.safe_escape(supporter_obj.first_name)}"
+        elif chat_obj.username:
+            chat_link = f"https://t.me/{chat_obj.username}/{msg_id}"
+            chat_display = f"<a href='{chat_link}'>{utils.safe_escape(chat_obj.title)}</a>"
+        else:
+            chat_display = utils.safe_escape(chat_obj.title)
+
     if action == "apr":
         if req_type in ["gban", "dgban"]:
             old_ban = await db.get_gban(target_id)
@@ -78,7 +90,7 @@ async def support_callback_handler(update: Update, context: ContextTypes.DEFAULT
 
             hashtag = "#GBANUPDATE" if old_ban else "#GBANNED"
             final_log = (f"<b>{hashtag}</b>\n"
-                         f"<b>Initiated From:</b> (Approved Request) [<code>{chat_id}</code>]\n\n"
+                         f"<b>Initiated From:</b> {chat_display} [<code>{chat_id}</code>]\n\n"
                          f"<b>User:</b> {user_link} [<code>{target_id}</code>]\n"
                          f"<b>Reason:</b> <code>{utils.safe_escape(reason)}</code>\n")
             
@@ -109,7 +121,7 @@ async def support_callback_handler(update: Update, context: ContextTypes.DEFAULT
         elif req_type == "ungban":
             if await db.remove_gban(target_id):
                 final_log = (f"<b>#UNGBANNED</b>\n"
-                             f"<b>Initiated From:</b> (Approved Request) [<code>{chat_id}</code>]\n\n"
+                             f"<b>Initiated From:</b> {chat_display} [<code>{chat_id}</code>]\n\n"
                              f"<b>User:</b> {user_link} [<code>{target_id}</code>]\n"
                              f"<b>Reason:</b> <code>{utils.safe_escape(reason)}</code>\n"
                              f"<b>Date:</b> <code>{curr_time}</code>\n"

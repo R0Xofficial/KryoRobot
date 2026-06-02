d7c76340a2d2ba508c868914ca144dd246294dfe
R0X <patryk.bieganski1003@gmail.com>
2026-06-02 10:57:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -819,6 +819,11 @@ async def addsudo_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("You are already the Master Owner.")
         return
 
+    if await db.is_support(target_id):
+        user_link = await utils.create_user_link(target_id, context)
+        await utils.send_safe_reply(update, context, f"User {user_link} [<code>{target_id}</code>] is <b>already</b> support.")
+        return
+
     if await db.is_sudo(target_id):
         user_link = await utils.create_user_link(target_id, context)
         await utils.send_safe_reply(update, context, f"User {user_link} [<code>{target_id}</code>] is <b>already</b> sudo.")
@@ -1190,6 +1195,11 @@ async def addsupport_command(update: Update, context: ContextTypes.DEFAULT_TYPE)
         return
     if target_id == OWNER_ID:
         await update.message.reply_text("You are already the Master Owner.")
+        return
+
+    if await db.is_sudo(target_id):
+        user_link = await utils.create_user_link(target_id, context)
+        await utils.send_safe_reply(update, context, f"User {user_link} [<code>{target_id}</code>] is <b>already</b> sudo.")
         return
 
     # Check if user is already in support

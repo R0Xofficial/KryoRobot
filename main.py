46a46e85a44b6b84ba5638f2fd601c15b4a2d003
R0X <patryk.bieganski1003@gmail.com>
2026-06-06 17:51:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -955,8 +955,13 @@ async def enforce_gban_command(update: Update, context: ContextTypes.DEFAULT_TYP
 
 @bot_command("stats")
 async def stats_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not await db.is_sudo(update.effective_user.id): return
-    if not await db.is_support(update.effective_user.id): return
+    admin = update.effective_user
+
+    is_sudo = await db.is_sudo(admin.id)
+    is_support = await db.is_support(admin.id)
+
+    if not (is_sudo or is_support):
+        return
     
     gbans = (await db.db_query("SELECT COUNT(*) FROM gbans", fetch="one"))[0]
     users = (await db.db_query("SELECT COUNT(*) FROM users", fetch="one"))[0]

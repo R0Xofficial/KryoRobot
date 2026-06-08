f3b17ff57ec82134d7304a2c200d07c4e6218347
R0X <patryk.bieganski1003@gmail.com>
2026-06-08 16:00:08 +0200

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -5,5 +5,6 @@ IMMUNE_IDS = [
     777000,        # Telegram Service Notifications
     1087968824,    # Telegram Service (Anonymous Admin)
     136817688,     # BotFather (example)
+    93372553,      # Official Telegram account
     # Add more IDs here manually
 ]

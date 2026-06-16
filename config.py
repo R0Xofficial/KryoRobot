cbea076180cc860250f25f1473861b436892cacf
R0X <patryk.bieganski1003@gmail.com>
2026-06-16 15:59:17 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,8 @@ DB_NAME = "gbanbotdata.db"
 APPEAL_CHAT_USERNAME = "@YourAppealChat"
 OWNER_ID = 123456789  # Master Admin
 LOG_CHAT_ID = -100123456789
+
+# HEARTBEAT CONFIG
+HEARTBEAT_ENABLED = False  # Change to True to enable
+HEARTBEAT_CHAT_ID = -100123456789 # ID of the chat to which the info should be sent (may be the same as LOG_CHAT_ID)
+HEARTBEAT_INTERVAL = 30   # Time in seconds
